3d81c58b4336079cd5eaeb8460b4d6ab735a2d3e
MWB <18778380867@163.com>
2024-10-10 22:03:11 +0800

    提交1

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        String name = "aaabbbccc";
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(name);
+        System.out.println(stringBuffer.substring(0,4));
+        Map map = new HashMap<>();
+    }
+}
